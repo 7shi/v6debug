cc59540f23dee51f92f1648edb9f68827696c11e
7shi <7shi@live.jp>
2011-03-27 20:07:19 +0900

    間接システムコールを再帰に修正

--- PDP11Lib/VM.Sys.vb
+++ PDP11Lib/VM.Sys.vb
@@ -3,24 +3,27 @@
         Dim t = Data(PC)
         PC += 2US
         Select Case t
-            Case 0
-                Dim arg = ReadUInt16(PC)
-                PC += 2US
-                Select Case Data(arg)
-                    Case 4 : SysWrite(arg) : Return
-                End Select
+            Case 0 ' indirect
+                Dim bak = PC + 2US
+                PC = ReadUInt16(PC)
+                ExecSys()
+                PC = bak
+                Return
             Case 1 ' exit
-                PC += 2US
                 HasExited = True
+                Return
+            Case 4 ' write
+                SysWrite()
                 Return
         End Select
         Abort("invaid sys")
     End Sub
 
-    Private Sub SysWrite(arg As UShort)
+    Private Sub SysWrite()
         Dim f = Regs(0)
-        Dim p = ReadUInt16(arg + 2)
-        Dim len = ReadUInt16(arg + 4)
+        Dim p = ReadUInt16(PC)
+        Dim len = ReadUInt16(PC + 2)
+        PC += 4US
         If f = 1 Then
             For i = 0 To len - 1
                 Dim b = Data(p + i)
