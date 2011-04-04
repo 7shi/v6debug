f60db31f417dafc8120697f0d3d1808f6a7edc46
7shi <7shi@live.jp>
2011-04-04 18:44:08 +0900

    システムコールの分岐ダミーをコメントで追加

--- PDP11Lib/VM.Sys.vb
+++ PDP11Lib/VM.Sys.vb
@@ -15,6 +15,46 @@ Partial Public Class VM
                 Next
             End If
             Select Case t
+                'Case 2 : _fork(args) : Return
+                'Case 3 : _read(args) : Return
+                'Case 6 : _close(args) : Return
+                'Case 7 : _wait(args) : Return
+                'Case 8 : _creat(args) : Return
+                'Case 9 : _link(args) : Return
+                'Case 10 : _unlink(args) : Return
+                'Case 11 : _exec(args) : Return
+                'Case 12 : _chdir(args) : Return
+                'Case 13 : _time(args) : Return
+                'Case 14 : _mknod(args) : Return
+                'Case 15 : _chmod(args) : Return
+                'Case 16 : _chown(args) : Return
+                'Case 17 : _break(args) : Return
+                'Case 18 : _stat(args) : Return
+                'Case 19 : _seek(args) : Return
+                'Case 20 : _getpid(args) : Return
+                'Case 21 : _mount(args) : Return
+                'Case 22 : _umount(args) : Return
+                'Case 23 : _setuid(args) : Return
+                'Case 24 : _getuid(args) : Return
+                'Case 25 : _stime(args) : Return
+                'Case 26 : _ptrace(args) : Return
+                'Case 28 : _fstat(args) : Return
+                'Case 30 : _smdate(args) : Return
+                'Case 31 : _stty(args) : Return
+                'Case 32 : _gtty(args) : Return
+                'Case 34 : _nice(args) : Return
+                'Case 35 : _sleep(args) : Return
+                'Case 36 : _sync(args) : Return
+                'Case 37 : _kill(args) : Return
+                'Case 38 : _switch(args) : Return
+                'Case 41 : _dup(args) : Return
+                'Case 42 : _pipe(args) : Return
+                'Case 43 : _times(args) : Return
+                'Case 44 : _prof(args) : Return
+                'Case 45 : _tiu(args) : Return
+                'Case 46 : _setgid(args) : Return
+                'Case 47 : _getgid(args) : Return
+                'Case 48 : _sig(args) : Return
                 Case 0 : _indir(args) : Return
                 Case 1 : _exit(args) : Return
                 Case 4 : _write(args) : Return
