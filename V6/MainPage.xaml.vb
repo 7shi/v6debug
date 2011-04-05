2d7accb4415753c8b4da0049becc288d2fdd9b1d
7shi <7shi@live.jp>
2011-04-05 15:23:48 +0900

    ソース一覧用のListBox1を追加

--- V6/MainPage.xaml.vb
+++ V6/MainPage.xaml.vb
@@ -9,20 +9,21 @@ Partial Public Class MainPage
     Private fs As New SLFileSystem("Tests")
 
     Private Class ProcArg
+        Public Srcs As String()
         Public Args As String()
         Public Verbose As Boolean
     End Class
 
     Public Sub New()
         InitializeComponent()
-        ReadFile("hello1", New ProcArg With {.Verbose = True})
-        addTest(True, "hello1")
-        addTest(True, "hello2")
-        addTest(True, "hello3")
-        addTest(True, "hello4")
-        addTest(True, "args", "test", "arg")
-        addTest(True, "printo")
-        addTest(False, "nm", "args")
+        Dim b = addTest(True, New String() {"hello1.c"}, "hello1")
+        addTest(True, New String() {"hello2.c"}, "hello2")
+        addTest(True, New String() {"hello3.c"}, "hello3")
+        addTest(True, New String() {"hello4.c"}, "hello4")
+        addTest(True, New String() {"args.c"}, "args", "test", "arg")
+        addTest(True, New String() {"printo.c"}, "printo")
+        addTest(False, New String() {"nm.c"}, "nm", "args")
+        btnTest_Click(b, Nothing)
     End Sub
 
     Public Sub Clear()
@@ -32,22 +33,30 @@ Partial Public Class MainPage
         txtBin.Text = ""
         txtTrace.Text = ""
         txtOut.Text = ""
+        ignore = True
+        ListBox1.Items.Clear()
+        ignore = False
     End Sub
 
-    Private Sub addTest(verbose As Boolean, t$, ParamArray args$())
-        Dim parg = New ProcArg With {.Args = args, .Verbose = verbose}
+    Private Function addTest(verbose As Boolean, srcs$(), t$, ParamArray args$()) As Button
+        Dim parg = New ProcArg With {.Srcs = srcs, .Args = args, .Verbose = verbose}
         Dim button = New Button With {.Content = t, .Tag = parg}
         AddHandler button.Click, AddressOf btnTest_Click
         menuStack.Children.Add(button)
-    End Sub
+        Return button
+    End Function
 
     Private Sub ReadFile(path$, parg As ProcArg)
+        Clear()
         Using s = fs.Open(path)
             ReadStream(s.Stream, GetFileName(path), parg)
         End Using
-        Using s = fs.Open(path + ".c")
-            txtSrc.Text = ReadText(s.Stream)
-        End Using
+        ignore = True
+        For Each src In parg.Srcs
+            ListBox1.Items.Add(src)
+        Next
+        ignore = False
+        ListBox1.SelectedIndex = 0
         txtSym.Text = VM.System(fs, "nm", path).Output
     End Sub
 
@@ -60,8 +69,8 @@ Partial Public Class MainPage
         btnSave.IsEnabled = True
     End Sub
 
-    Private Sub btnOpen_Click(sender As Object, e As RoutedEventArgs)
-        Dim ofd = New OpenFileDialog()
+    Private Sub btnOpen_Click(sender As Object, e As RoutedEventArgs) Handles btnOpen.Click
+        Dim ofd = New OpenFileDialog
         If ofd.ShowDialog() <> True Then Return
 
         Clear()
@@ -70,7 +79,7 @@ Partial Public Class MainPage
             If fi.Length >= 64 * 1024 Then
                 Throw New Exception("ファイルが大き過ぎます。上限は64KBです。")
             End If
-            Using fs = ofd.File.OpenRead()
+            Using fs = ofd.File.OpenRead
                 ReadStream(fs, ofd.File.Name, Nothing)
             End Using
         Catch ex As Exception
@@ -79,11 +88,11 @@ Partial Public Class MainPage
         End Try
     End Sub
 
-    Private Sub btnSave_Click(sender As Object, e As RoutedEventArgs)
-        Dim sfd = New SaveFileDialog()
+    Private Sub btnSave_Click(sender As Object, e As RoutedEventArgs) Handles btnSave.Click
+        Dim sfd = New SaveFileDialog
         If sfd.ShowDialog() <> True Then Return
 
-        Using fs = sfd.OpenFile()
+        Using fs = sfd.OpenFile
             fs.Write(aout.Data, 0, aout.Data.Length)
         End Using
     End Sub
@@ -94,11 +103,11 @@ Partial Public Class MainPage
 
         Dim cur = Cursor
         Cursor = Cursors.Wait
-        ReadFile(button.Content.ToString(), CType(button.Tag, ProcArg))
+        ReadFile(button.Content.ToString, CType(button.Tag, ProcArg))
         Cursor = cur
     End Sub
 
-    Private Sub comboBox1_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+    Private Sub comboBox1_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles comboBox1.SelectionChanged
         Run()
     End Sub
 
@@ -110,8 +119,8 @@ Partial Public Class MainPage
         aout.UseOct = comboBox1.SelectedIndex = 1
         Dim vm = New VM(aout, fs, parg.Verbose)
         vm.Run(parg.Args)
-        txtDis.Text = aout.GetDisassemble()
-        txtBin.Text = aout.GetDump()
+        txtDis.Text = aout.GetDisassemble
+        txtBin.Text = aout.GetDump
         txtTrace.Text = vm.Trace
         txtTrace.SelectionStart = txtTrace.Text.Length
         txtOut.Text = vm.Output
@@ -123,4 +132,18 @@ Partial Public Class MainPage
         Dim p = path.LastIndexOf(CChar("/"))
         Return If(p < 0, path, path.Substring(p + 1))
     End Function
+
+    Private ignore As Boolean
+
+    Private Sub ListBox1_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles ListBox1.SelectionChanged
+        If ignore Then Return
+        Dim sel = ListBox1.SelectedItem
+        If sel IsNot Nothing Then
+            Using s = fs.Open(sel.ToString)
+                txtSrc.Text = ReadText(s.Stream)
+            End Using
+        Else
+            txtSrc.Text = ""
+        End If
+    End Sub
 End Class
