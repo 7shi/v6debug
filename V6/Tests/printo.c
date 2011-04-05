6a54adb6e1b8038658735aa439c9767ad45e5ff2
7shi <7shi@live.jp>
2011-04-05 12:03:25 +0900

    putchar()によりバイナリを単純化

--- V6/Tests/printo.c
+++ V6/Tests/printo.c
@@ -6,10 +6,10 @@ main()
 printo(v)
 {
     int i;
-    printf("%c", v < 0 ? '1' : '0');
+    putchar(v < 0 ? '1' : '0');
     for(i = 0; i < 5; i++)
     {
-        printf("%c", ((v >> 12) & 7) + '0');
+        putchar(((v >> 12) & 7) + '0');
         v =<< 3;
     }
 }
